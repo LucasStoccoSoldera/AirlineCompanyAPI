ad14ddc8ba8779f7849f8be8e35a7be6e8190f33
Lucas Stocco Soldera <67830589+LucasStoccoSoldera@users.noreply.github.com>
2024-02-03 13:16:37 -0300

    Created default Json response

---
+++ AirlineCompanyAPI/Dtos/Responses/DefaultJsonResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace AirlineCompanyAPI.Dtos.Responses
+{
+    internal class DefaultJsonResponse<DataType>
+    {
+        public bool Success { get; set; }
+        public required string Message { get; set; }
+        public int InternalCode { get; set; }
+        public DataType? Data { get; set; }
+    }
+}
